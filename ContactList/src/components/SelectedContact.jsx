c93669b60a1999b58b1b7d0b2bf72020c11690e8
patteesolomon <theshiningkillerrashakublade@outlook.com>
2023-07-07 23:13:25 -0500

    Update SelectedContact.jsx
    secure

--- ContactList/src/components/SelectedContact.jsx
+++ ContactList/src/components/SelectedContact.jsx
@@ -5,7 +5,7 @@ const SelectedContact = ({ selectedContactId, setSelectedContactId }) => {
     const [contact, setContact] = useState([]);
     useEffect(() => {
         async function getter() {
-            const response = await fetch(`http://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`);
+            const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`);
             const result = await response.json();
             setContact(result);
         }
